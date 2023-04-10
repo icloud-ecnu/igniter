7b3e20a242b3e216bf5fbb6107f319047b125bb4
echozqn <1870571113@qq.com>
2023-04-10 13:33:27 +0800

    codeocean update

--- i-Gniter/Algorithm/igniter-algorithm.py
+++ i-Gniter/Algorithm/igniter-algorithm.py
@@ -20,11 +20,11 @@ class Context:
     powerp = []
     idlef = []
 
+
 class Model:
     name = "alexnet"
     kernels = 29
     baseidle = 0.1
-    # act_popt[0-4] k1-k5
     act_popt = []
     l2cache = 0
     power_popt = []
@@ -39,28 +39,34 @@ context = Context()
 
 
 def loadprofile(path):
+    """
+    This function loads profile data from a JSON file and returns it as a dictionary.
+    :param path: A string representing the path to the JSON file.
+    :return: A dictionary containing the loaded profile data.
+    """
     profile = {}
     with open(path, "r") as file:
         for line in file:
-            # print(line)
             data = json.loads(line)
             for key in data:
                 if (key not in profile):
                     profile[key] = data[key]
                 else:
                     profile[key].update(data[key])
-
     return profile
 
 
 def p_f(po, a):
-    # power to frequency when frequency>1530
     return a * (po - context.power) + context.frequency
 
 
 def power_frequency(power, frequency):
-    # the idle percent of VGG19 is very low, the GPU power of VGG-19 grows linearly.
-    # p_f(power,popt[0])
+    """
+    This function calculates the power frequency of a `VGG-19` model given the power and frequency data. The idle percent of the `VGG-19` model is very low and its GPU power grows linearly.
+    :param power: A list of power values for the VGG-19 model.
+    :param frequency: A list of frequency values for the VGG-19 model.
+    :return: An array of optimal parameter values for the power frequency equation
+    """
     k = 0
     l = 0
     for i in range(len(frequency)):
@@ -84,8 +90,12 @@ def idle_f(n_i, alpha_sch, beta_sch):
 
 
 def idletime(idletime, frequency):
-    # use : idle_f(number of inference workloads, popt[0], popt[1])
-    # vgg19
+    """
+    This function calculates the idle time based on the input data of idle time and frequency.
+    :param idletime: A list of idle time data points.
+    :param frequency: A list of frequency data points corresponding to the idle time data points.
+    :return: An array of two fitted parameters using the curve_fit function based on the idle time data.
+    """
     l = len(idletime)
     vgg19 = [0] * l
     for i in range(l):
@@ -105,10 +115,15 @@ def activetime_func(x, k1, k2, k3, k4, k5):
 
 
 def activetime(gpulatency, frequency, baseidletime):
-    # activetime_func([b,th], popt[0], popt[1], popt[2], popt[3], popt[4])
+    """
+    This function calculates the active time for a GPU given the GPU latency, frequency, and base idle time.
+    :param gpulatency: A 2D list of GPU latency for different batch sizes and threads.
+    :param frequency: A 2D list of GPU frequency for different batch sizes and threads.
+    :param baseidletime: The base idle time for the GPU.
+    :return: The parameters obtained from the curve fitting.
+    """
     activelatency = []
     x = [[], []]
-    # batchsize thread
     for i in range(len(context.batchsize)):
         for j in range(len(context.thread)):
             tmp = gpulatency[i][j]
@@ -126,11 +141,18 @@ def ability_cp(x, k, b):
     return k * x + b
 
 
-# l2caches 对应的应该是[b,r] =  [[1, 10] [16, 50] [32, 100]]
 def power_l2cache(power, gpulatency, baseidletime, idlepower, l2caches, frequency):
-    # power gpulatecny t50
-    # power[batchsize][thread]
-    # popt act_popt
+    """
+    This function calculates the optimal values of the parameters popt_p and popt_c for a given input data, using a non-linear curve fitting method.
+    :param power: A 3x3 list of power values, where power[i][j] corresponds to the power consumed by i-th batch size and j-th thread.
+    :param gpulatency: A 3x3 list of GPU latency values, where gpulatency[i][j] corresponds to the latency of i-th batch size and j-th thread.
+    :param baseidletime: A float value representing the base idle time.
+    :param idlepower: A float value representing the power consumed in idle state.
+    :param l2caches: A 3x2 list of L2 cache values, where l2caches[i] corresponds to the L2 cache sizes of i-th batch size, i.e., [1, 10], [16, 50], and [32, 100].
+    :param frequency: A 3x3 list of GPU frequency values, where frequency[i][j] corresponds to the frequency of i-th batch size and j-th thread.
+    :return: popt_p: A tuple representing the optimal values of the parameters for power curve.
+             popt_c: A tuple representing the optimal values of the parameters for L2 cache curve.
+    """
     batch = [1, 16, 32]
     resource = [10, 50, 100]
     ability = []
@@ -144,15 +166,21 @@ def power_l2cache(power, gpulatency, baseidletime, idlepower, l2caches, frequenc
             if frequency[i][j] > context.frequency - 1:
                 ability_p.append((1000 * b) / (gpulatency[i][j] - baseidletime))
                 ypower.append(power[i][j] - idlepower)
-    # print(ypower)
     popt_p, pcov_p = curve_fit(ability_cp, ability_p, ypower)
-    popt_c, pcov_c = curve_fit(ability_cp, [ability[0], ability[4], ability[8]], l2caches) # [[1, 10] [16, 50] [32, 100]]
+    popt_c, pcov_c = curve_fit(ability_cp, [ability[0], ability[4], ability[8]],
+                               l2caches)  # [[1, 10] [16, 50] [32, 100]]
 
     return popt_p, popt_c
 
 
-def predict(models, batchsize, thread):  # model类型是Model类,batch,resource
-    # batchsize [[throughput] ,[inference latency]]
+def predict(models, batchsize, thread):
+    """
+    This function predicts the inference latency and throughput of each model based on the models placed on the current GPU.
+    :param models: A list of Model objects containing details of the models such as idle time, activation function, power consumption, L2 cache, etc.
+    :param batchsize: A list of integers representing batch size for each model.
+    :param thread: A list of integers representing the number of threads used for each model.
+    :return: A list containing two sub-lists. The first sub-list contains throughput for each model,and the second sub-list contains inference latency for each model.
+    """
     p = len(models)
     ans = [[], []]
     increasing_idle = 0
@@ -190,28 +218,23 @@ def predict(models, batchsize, thread):  # model类型是Model类,batch,resource
     return ans
 
 
-def error(act, pre):
-    err = []
-    for i in range(len(act)):
-        err.append(abs(act[i] - pre[i]) / act[i])
-        # print(act[i],pre[i],err[i])
-    return err
-
-
 def durationps(openfilepath):
+    """
+    This function calculates the average duration and standard deviation of GPU and inference latency for a given log file.
+    :param openfilepath: A string representing the path of the log file to be read.
+    :return: A tuple containing two lists. The first list contains the mean duration for GPU and inference latency, respectively. The second list contains the standard deviation for GPU and inference latency, respectively.
+             If the log file contains less than 10 valid entries, the function raises a "Duration time is too short!" warning message.
+    """
     with open(openfilepath, encoding='utf-8') as f:
         log = f.readlines()
         gpulatency = []
         inferencelatency = []
         for i in range(1, len(log) - 1):
             j = eval(log[i][1:])
-            # j = json.loads(log[i])
             if (j["startComputeMs"] > 10000 and j["endComputeMs"] < 20000):
-                # latencyMs computeMs
                 gpulatency.append(j["computeMs"])
                 inferencelatency.append(j["latencyMs"])
         if len(gpulatency) > 10:
-            # print(sum/n,sum2/n)
             return ([np.mean(gpulatency), np.mean(inferencelatency)], [np.std(gpulatency), np.std(inferencelatency)])
         else:
             print("Duration time is too short!")
@@ -225,9 +248,20 @@ def r_total(ra_j):
 
 
 def alloc_gpus(inference, batch, slo, ra_ij, r_lower, w, j):
+    """
+    Allocate GPUs for the given workloads based on the resources available and the Service Level Objectives (SLOs) of the workloads.
+    :param inference: A list of inference models.
+    :param batch: A list of batch sizes for each inference model.
+    :param slo: A list of Service Level Objectives (SLOs) for each inference model.
+    :param ra_ij: A nested list of tuples, where each tuple represents the workload and the allocated resources for that workload on a specific GPU.
+    :param r_lower: The lower bound of resources that can be allocated to a workload
+    :param w: The workload index to be allocated.
+    :param j: The index of the GPU to allocate the workload to.
+    :return: ra_ij: An updated list of allocated resources for each GPU after allocating the workload
+    """
     flag = 1
     ra_ij[j].append([w, r_lower])
-    if r_total(ra_ij[j]) > 100:  # 如果该GPU上的所有模型的资源之和超过了100
+    if r_total(ra_ij[j]) > 100:  # If the sum of the resources of all models on that GPU exceeds 100
         return ra_ij
     addinferece = []
     addbatch = []
@@ -239,7 +273,9 @@ def alloc_gpus(inference, batch, slo, ra_ij, r_lower, w, j):
         addbatch.append(batch[infwork[0]])
         addresource.append(infwork[1])
 
-    while np.sum(addresource) <= 100 and flag == 1:  # 增加一个负载之后，可能预测之后其他模型的latency发生变化，所以就要一直修改，直到所有模型的latency都满足要求
+    while np.sum(addresource) <= 100 and flag == 1:
+        # After adding a load, the latency of other models may be predicted to change afterwards, so you have to keep
+        # modifying until the latency of all models meet the requirements
         flag = 0
         latency = predict(addinferece, addbatch, addresource)[1]
         for i in range(num_workloads):
@@ -253,10 +289,15 @@ def alloc_gpus(inference, batch, slo, ra_ij, r_lower, w, j):
 
 
 def algorithm(inference, slo, rate):
+    """
+    This function determines how to place the DNN inference load and allocates GPU resources based on the user's input workload, latency, and request rate.
+    :param inference: A list of models to allocate to GPUs.
+    :param slo: A list of target SLOs (Service Level Objectives) for each model in the 'inference' list.
+    :param rate: A list of inference request rates for each model in the 'inference' list.
+    :return: None
+    """
     for i in range(len(inference)):
-        print(inference[i].name,slo[i],rate[i])
-    # inference: models
-
+        print(inference[i].name, slo[i], rate[i])
     # Initialize:
     slo = [i / 2 for i in slo]
     m = len(inference)
@@ -279,7 +320,7 @@ def algorithm(inference, slo, rate):
     for i in range(len(r_lower)):
         if r_lower[i][1] > 100:
             print("workload is too large!")
-            # 给100%资源，遍历batchsize
+            # Give 100% resources, traverse batchsize
             max_batch = -1
             new_rate = 0
             id = r_lower[i][0]
@@ -291,15 +332,14 @@ def algorithm(inference, slo, rate):
                 else:
                     break
             if max_batch == -1:
-                print("GPU 无法满足该负载")
+                print("GPU can't handle the load")
             else:
                 new_rate = math.ceil(new_rate * 0.9)
                 while rate[id] > new_rate:
                     result.append([id, inference[id].name, max_batch, 100, new_rate, slo[id]])
                     rate[id] -= new_rate
 
-
-    g = 1  # 表示GPU的数量
+    g = 1  # Indicates the number of GPUs
     arrivate = [i / 1000 for i in rate]
     for i in range(m):
         inf = inference[i]
@@ -312,13 +352,13 @@ def algorithm(inference, slo, rate):
 
     # sorted
     r_lower.sort(key=lambda x: x[1], reverse=True)
-    # 放置操作
+    # Placement operation
     for ti in r_lower:
-        i = ti[0]  # i表示第i个模型
+        i = ti[0]  # i denotes the i-th model
         r_l = ti[1]
-        ra_ij = copy.deepcopy(resource)  # 表示把i这个模型放置到j这个GPU上被分配的GPU resource
-        q = -1  # q表示i这个模型被分配到的GPU id
-        r_inter_min = 101  # 最小的干扰
+        ra_ij = copy.deepcopy(resource)  # denotes the GPU resource assigned to the model i placed on the GPU j
+        q = -1  # q denotes the GPU id that i the model is assigned to
+        r_inter_min = 101  # Initialize minimal interference
         for j in range(g):
             ra_ij = alloc_gpus(inference, batch, slo, ra_ij, r_l, i, j)
             r_inter = r_total(ra_ij[j]) - r_total(resource[j])
@@ -366,15 +406,15 @@ def algorithm(inference, slo, rate):
             json.dump(conf, f)
         gpuid += 1
 
+
 if __name__ == '__main__':
     profile = loadprofile("./config")
 
     context.frequency = profile["hardware"]["maxGPUfrequency"]
     context.power = profile["hardware"]["maxGPUpower"]
-    context.unit = profile["hardware"]["unit"] # 100 / (SM / 2)
-    context.bandwidth = profile["hardware"]["bandwidth"] # PCIe
+    context.unit = profile["hardware"]["unit"]  # 100 / (SM / 2)
+    context.bandwidth = profile["hardware"]["bandwidth"]  # PCIe
     context.kernels = []
-
 
     context.idlepower = profile["hardware"]["idlepower"]
     context.powerp = power_frequency(profile["hardware"]["power"], profile["hardware"]["frequency"])
@@ -388,9 +428,6 @@ if __name__ == '__main__':
     # workloads = [models[0], models[1], models[2]]
     # SLOs = [15, 40, 60]
     # rates = [500, 400, 200]
-    """
-    python3 igniter-algorithm.py -s 
-    """
 
     parse = argparse.ArgumentParser()
     parse.add_argument(
@@ -410,7 +447,7 @@ if __name__ == '__main__':
         rates = []
         model_list = []
         for model_config in FLAGS.model_slo_rate:
-            model,slo,rate = model_config.split(":")
+            model, slo, rate = model_config.split(":")
             model_list.append(model)
             SLOs.append(int(slo))
             rates.append(int(rate))
@@ -435,11 +472,3 @@ if __name__ == '__main__':
 
     workloads = [models[i] for i in range(len(SLOs))]
     algorithm(workloads, SLOs, rates)
-
-"""
-alexnet_dynamic 15 500
-resnet50_dynamic 40 400
-vgg19_dynamic 60 200
-
-python3 igniter-algorithm.py -m alexnet:15:500 -m vgg19:60:200 -m resnet50:40:400 
-"""
